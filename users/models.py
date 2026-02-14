d853afed63b3b1f8ad04a52a7b552c63d80b2045
Deniz Pandar <denizpandar@gmail.com>
2026-02-14 13:58:07 +0000

    fix(models): correct address field spelling in UserProfile

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, blank=True)
-    adress = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.user.username
