6d1910c9d5ff401723e34ee130c5c9ef456ec78e
Deniz Pandar <denizpandar@gmail.com>
2026-04-07 22:44:57 +0100

    Add registration form and add country to users model

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, blank=True)
     address = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
