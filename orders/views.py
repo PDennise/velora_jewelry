064f1a72f97c004756558e2cb015af7ae0de1a96
Deniz Pandar <denizpandar@gmail.com>
2026-05-07 18:57:01 +0100

    Add order detail template, view and URL path

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Order
 
 # Create your views here.
+@login_required
+def order_detail(request, order_id):
+    order = get_object_or_404(
+        Order,
+        id=order_id,
+        user=request.user                                   # Users cannot see other users’ data
+    )
+
+    return render(request, "orders/order_detail.html", {
+        "order": order
+    })
