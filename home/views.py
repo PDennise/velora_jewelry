bdd52bed27de9e451e49528993911a03c302193a
Deniz Pandar <denizpandar@gmail.com>
2026-05-25 23:26:20 +0100

    Fix home views and remove unused Category model from home app and fix home admin and views imports after Category model removal

--- home/views.py
+++ home/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
-from .models import Category
 from shop.models import Product
 
 # Create your views here.
 def homepage(request):
-
-    categories = Category.objects.all()
 
     featured_products = Product.objects.filter(
         featured=True
     )[:4]
 
     context = {
-        'categories': categories,
         'featured_products': featured_products,
     }
     return render(request, 'home/home.html', context)
